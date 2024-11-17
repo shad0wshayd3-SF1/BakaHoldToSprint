83b3b3bd91b198bdc3ad37a3fff277208d1162b0
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2024-11-16 18:32:12 -0700

    refactor: cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,38 @@
-class Hooks
+namespace Hooks
 {
-public:
-	static void Install()
+	// PlayerControls::SprintHandler
+	namespace hkSprintHandler
 	{
-		// PlayerControls::SprintHandler
-		hkSprintHandler::Install();
-	}
-
-private:
-	class hkSprintHandler
-	{
-	public:
-		static void Install()
+		static bool SprintHandler(RE::ButtonEvent* a_event)
 		{
-			static REL::Relocation<std::uintptr_t> target{ REL::ID(129187), 0x0C };
-			auto& trampoline = SFSE::GetTrampoline();
-			trampoline.write_call<5>(target.address(), SprintHandler);
-		}
-
-	private:
-		// clang-format off
-		struct ButtonEventImpl
-		{
-		public:
-			std::byte pad00[0x48];  // 00
-			float value;            // 48
-			float heldDownSecs;     // 4C
-			std::byte pad50[0x10];  // 50
-		};
-		static_assert(sizeof(ButtonEventImpl) == 0x60);
-
-		// clang-format on
-
-		static bool SprintHandler(ButtonEventImpl* a_event)
-		{
-			if (a_event->value > 0.0f && a_event->heldDownSecs == 0.0f)
+			if (a_event->value > 0.0f &&
+			    a_event->heldDownSecs == 0.0f)
 			{
 				return true;
 			}
-			else if (a_event->value == 0.0f && a_event->heldDownSecs > 0.0f)
+			else if (a_event->value == 0.0f &&
+			         a_event->heldDownSecs > 0.0f)
 			{
-				static REL::Relocation<void**> PlayerCharacter{ REL::ID(865059) };
-				auto flag = REL::stl::adjust_pointer<std::uint8_t>(*PlayerCharacter.get(), 0x1124);
-				(*flag) = (*flag) & ~4;
+				auto PlayerCharacter = RE::PlayerCharacter::GetSingleton();
+				auto flag = REL::stl::adjust_pointer<std::uint8_t>(PlayerCharacter, 0x1124);
+				*flag &= ~4;
 			}
 
 			return false;
 		}
-	};
-};
+
+		static void Install()
+		{
+			static REL::Relocation target{ REL::ID(129187), 0x0C };
+			target.write_call<5>(SprintHandler);
+		}
+	}
+
+	static void Install()
+	{
+		hkSprintHandler::Install();
+	}
+}
 
 namespace
 {
