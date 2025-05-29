2474ffd347594802177be9d97ae1c2b903179945
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2025-05-29 08:56:38 -0600

    fix: aiming thing

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,20 @@ namespace Hooks
 	private:
 		static void SprintHandler(void* a_this, RE::ButtonEvent* a_event)
 		{
-			if (a_event->value > 0.0f &&
-			    a_event->heldDownSecs == 0.0f)
-			{
-				auto PlayerCharacter = RE::PlayerCharacter::GetSingleton();
-				auto flag = REX::ADJUST_POINTER<std::uint8_t>(PlayerCharacter, 0x1124);
-				*flag &= 4;
-			}
-			else if (a_event->value == 0.0f &&
-			         a_event->heldDownSecs > 0.0f)
-			{
-				auto PlayerCharacter = RE::PlayerCharacter::GetSingleton();
-				auto flag = REX::ADJUST_POINTER<std::uint8_t>(PlayerCharacter, 0x1124);
-				*flag &= ~4;
-			}
-
 			_Hook0(a_this, a_event);
+
+			if (auto PlayerCharacter = RE::PlayerCharacter::GetSingleton())
+			{
+				auto flag = REX::ADJUST_POINTER<std::uint8_t>(PlayerCharacter, 0x1124);
+				if (*flag & 4)
+				{
+					if (a_event->value == 0.0f &&
+						a_event->heldDownSecs > 0.0f)
+					{
+						*flag &= ~4;
+					}
+				}
+			}
 		}
 
 		inline static REL::HookVFT _Hook0{ RE::VTABLE::PlayerControls__SprintHandler[0], 0x08, SprintHandler };
